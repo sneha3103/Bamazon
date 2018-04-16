29f12cc6f3a9614d381581c6375d33e3791639a1
Sneha Dama <snehadama@Snehas-Air.lan>
2018-04-16 14:39:34 -0500

    Added a display products function to show the products, also updated the database when the user selects their product quantity and consoled out the total cost

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,11 +14,64 @@ connection.connect(function(err){
     if(err) throw(err);
     console.log(`connected as id ${connection.threadId}`);
 
-    connection.query("SELECT * FROM products", function(err, res){
-        if(err) throw err;
+    displayProducts();
+});
+
+//Querying database, and displaying products on the screen and then run the prompt customer function
+function displayProducts () {
+     connection.query("SELECT * FROM products", function (err, res) {
+        if (err) console.log(err);
 
         for(var i =0; i< res.length; i++){
             console.log(res[i].item_id + "|" + res[i].product_name + " |" + res[i].price);
         }
-    });
-});
+        promptCustomer(res);
+    })
+}
+
+//Created a function to prompt the customer to select the ID of their choice and how many units they would like to buy
+function promptCustomer(res) {
+    
+        inquirer.prompt([{
+                type: "input",
+                message: "Input the ID of the item you are interested in!",
+                name: "itemID",
+            },
+            {
+                type: "input",
+                message: "How many units of the product would you like to buy?",
+                name: "quantity",
+            },
+        ]).then(function (answers) {
+            var selectedProduct;
+
+            for (var i = 0; i < res.length; i += 1) {
+                if (res[i].item_id === parseInt(answers.itemID)) {
+                    selectedProduct = res[i];
+                    console.log(res[i]);
+                }
+            };
+            // console.log(typeof selectedProduct.stock_quantity);
+            // console.log(typeof answers.quantity);
+            var userQuantity = parseInt(answers.quantity);
+           
+            if(selectedProduct.stock_quantity < userQuantity) {
+                console.log("Insufficient Quantity");
+            } else {
+                //selectedProduct.price is coming from the database
+                var cost = userQuantity * selectedProduct.price 
+                //.toFixed limits decimal value to 2 spots.
+                console.log("Your total cost is: $ " + cost.toFixed(2));
+
+                //To update database
+                connection.query("UPDATE products SET ? WHERE ?", [{"stock_quantity": selectedProduct.stock_quantity - userQuantity} , {"item_id": answers.itemID}] ,function (err, res) {
+                    if(err) {
+                        console.log("There was an error. Purchase not made.");
+                        throw err;
+                    } 
+                        console.log("Thanks for your purchase!");
+                        
+                })
+            }
+        });
+    };
